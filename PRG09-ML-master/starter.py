5cf82ba13d376eaa9a1ceb2f4413751f70a0ca28
DIonGrimm <31693758+DionGrimm@users.noreply.github.com>
2020-03-30 19:06:56 +0200

    supervised

--- PRG09-ML-master/starter.py
+++ PRG09-ML-master/starter.py
@@ -15,10 +15,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-
 
 def extract_from_json_as_np_array(key, json_data):
     """ helper functie om data uit de json te halen en om te zetten naar numpy array voor sklearn"""
@@ -87,40 +85,80 @@ def unsupervisedLearning():
     plt.show()
 
 # SUPERVISED LEARNING
+def supervisedLearning(logistic=True):
+    """logistic or decision trees"""
+    # haal data op voor classificatie
+    classification_training = data.classification_training()
 
-# haal data op voor classificatie
-classification_training = data.classification_training()
+    # extract de data x = array met waarden, y = classificatie 0 of 1
+    X = extract_from_json_as_np_array("x", classification_training)
+    Y = extract_from_json_as_np_array("y", classification_training)
 
-# extract de data x = array met waarden, y = classificatie 0 of 1
-X = extract_from_json_as_np_array("x", classification_training)
-
-# dit zijn de werkelijke waarden, daarom kan je die gebruiken om te trainen
-Y = extract_from_json_as_np_array("y", classification_training)
-
-
-# TODO: leer de classificaties
-
-# TODO: voorspel na het trainen de Y-waarden (je gebruikt hiervoor dus niet meer de
-#       echte Y-waarden, maar enkel de X en je getrainde classifier) en noem deze
-#       bijvoordeeld Y_predict
-
-# TODO: vergelijk Y_predict met de echte Y om te zien hoe goed je getraind hebt
+    # TODO: leer de classificaties
 
 
-# haal data op om te testen
-classification_test = data.classification_test()
-# testen doen we 'geblinddoekt' je krijgt nu de Y's niet
-X_test = extract_from_json_as_np_array("x", classification_test)
+    # TODO: voorspel na het trainen de Y-waarden (je gebruikt hiervoor dus niet meer de
+    #       echte Y-waarden, maar enkel de X en je getrainde classifier) en noem deze
+    #       bijvoordeeld Y_predict
 
-# TODO: voorspel na nog een keer de Y-waarden, en plaats die in de variabele Z
-#       je kunt nu zelf niet testen hoe goed je het gedaan hebt omdat je nu
-#       geen echte Y-waarden gekregen hebt.
-#       onderstaande code stuurt je voorspelling naar de server, die je dan
-#       vertelt hoeveel er goed waren.
+    # TODO: vergelijk Y_predict met de echte Y om te zien hoe goed je getraind hebt
 
-Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoeveel procent er goed is?
+    if logistic:
+        classifier = LogisticRegression()
+    else:
+        classifier = DecisionTreeClassifier()
 
-# stuur je voorspelling naar de server om te kijken hoe goed je het gedaan hebt
-classification_test = data.classification_test(Z.tolist()) # tolist zorgt ervoor dat het numpy object uit de predict omgezet wordt naar een 'normale' lijst van 1'en en 0'en
-print("Classificatie accuratie (test): " + str(classification_test))
+    # Train
+    classifier.fit(X, Y)
 
+    # Y predictions
+    Y_predict = classifier.predict(X)
+
+    # Print accuracu between Y test and predictions
+    print("Trainings accuracy:", accuracy_score(Y, Y_predict))
+
+    # TODO: voorspel na nog een keer de Y-waarden, en plaats die in de variabele Z
+    #       je kunt nu zelf niet testen hoe goed je het gedaan hebt omdat je nu
+    #       geen echte Y-waarden gekregen hebt.
+    #       onderstaande code stuurt je voorspelling naar de server, die je dan
+    #       vertelt hoeveel er goed waren.
+
+    # haal data op om te testen
+    classification_test = data.classification_test()
+    # testen doen we 'geblinddoekt' je krijgt nu de Y's niet
+    X_test = extract_from_json_as_np_array("x", classification_test)
+
+    Z = classifier.predict(X_test)
+    classification_test = data.classification_test(Z.tolist()) # tolist zorgt ervoor dat het numpy object uit de predict omgezet wordt naar een 'normale' lijst van 1'en en 0'en
+
+    # Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoeveel procent er goed is? rond de 50%
+
+    # stuur je voorspelling naar de server om te kijken hoe goed je het gedaan hebt
+    print("Classificatie accuratie (test): " + str(classification_test))
+
+    # Plot trainingsdata
+    x = X[...,0]
+    y = X[...,1]
+
+    # True data graph
+    fig1 = plt.figure(1)
+
+    for i in range(len(x)):
+        plt.plot(x[i], y[i], 'k.', markersize=8)
+        # Plot true data in red
+        if (Y[i] == 1): plt.plot(x[i], y[i], 'r.', markersize=8)
+
+    plt.axis([min(x), max(x), min(y), max(y)])
+
+    # Predicted data graph
+    fig2= plt.figure(2)
+
+    for i in range(len(x)):
+        plt.plot(x[i], y[i], 'k.', markersize=8)
+        # Plot true data in red
+        if (Y_predict[i] == 1): plt.plot(x[i], y[i], 'r.', markersize=8)
+
+    plt.axis([min(x), max(x), min(y), max(y)])
+    plt.show()
+
+supervisedLearning(False)
